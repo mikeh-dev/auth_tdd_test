f8b85fa8b821dcfa8ea9856862423a110bcf5c3a
mikeh112@hotmail.com <mikeh112@hotmail.com>
2024-10-27 22:46:41 +0000

    Began to add password reset feature

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,6 @@
 class HomeController < ApplicationController
+  
   def index
-    if session[:user_id]
-      @user = User.find(session[:user_id])
-    end
   end
 
   def about
--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,10 @@ Rails.application.routes.draw do
 
   delete "logout", to: "sessions#destroy"
 
+  get "password", to: "passwords#edit", as: :edit_password
+  patch "password", to: "passwords#update"
+
+  get "password/reset", to: "password_resets#new"
+  post "password/reset", to: "password_resets#create"
+
 end
