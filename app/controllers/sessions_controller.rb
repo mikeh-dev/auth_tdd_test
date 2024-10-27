a2c339dce4e80f5ddeaf7590c65435c264c396de
mikeh112@hotmail.com <mikeh112@hotmail.com>
2024-10-27 20:41:16 +0000

    Sign Up, Signin, logout now working

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,5 +8,13 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_path, notice: "Logged in successfully"
+    else
+      flash[:alert] = "Invalid email or password"
+      render :new, status: :unprocessable_entity
+    end
   end
 end
